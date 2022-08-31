553b99228557d50713d2de22a895e38f2d8c8015
sebarray <sebarray@gmail.com>
2022-08-31 05:20:37 -0300

    logs

--- handler/manager.go
+++ handler/manager.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/sebarray/wiselink/routes/v1"
 )
@@ -11,8 +12,11 @@ import (
 func Manager() {
 
 	e := echo.New()
+	e.Use(middleware.Logger())
 	r := e.Group("/event")
+	r.Use(middleware.Logger())
 	routesSuscribe := e.Group("/suscribe")
+	routesSuscribe.Use(middleware.Logger())
 	routes.Event(r)
 	routes.Suscribe(routesSuscribe)
 	routes.Login(e)
