7df75891ea39ad0520ffa818dad8c6d298a847aa
SebastianDemello <sebastian.demello@landscape.cl>
2022-09-03 19:32:11 -0300

    cors

--- routes/v1/user.go
+++ routes/v1/user.go
@@ -2,10 +2,13 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/sebarray/wiselink/config"
 	"github.com/sebarray/wiselink/controller"
 )
 
 func Login(e *echo.Echo) {
+	e.Use(middleware.CORSWithConfig(config.GetCors()))
 	e.POST("/login", controller.Login)
 	e.POST("/register", controller.PostUser)
 }
